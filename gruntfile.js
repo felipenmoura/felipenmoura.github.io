0556fe01d5ddcefb58194bbc89ef38315bf9b0a0
Felipe N. Moura <felipenmoura@gmail.com>
2015-01-15 23:05:16 -0200

    [.] working on grunt generator

--- gruntfile.js
+++ gruntfile.js
@@ -1,32 +1,83 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-      
-    sass: {
-        dist: {
+    var NunJucks = require('nunjucks');
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+//        nunjucks: {
+//            options: {
+//            },
+//            precompile : {
+//                baseDir: 'views/',
+//                src: 'views/*',
+//                dest: 'static/js/templates.js',
+//                options: {
+//                    env: require('./nunjucks-environment'),
+//                    name: function(filename) {
+//                        return 'foo/' + filename;
+//                    }
+//                }
+//            },
+//        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files:  [{
+                    expand: true,
+                    cwd: 'scss',
+                    src: ['*.scss'],
+                    dest: 'styles',
+                    ext: '.css'
+                }]
+            }
+        },
+        
+        scsslint: {
+            allFiles: [
+                'scss/**/*',
+            ],
             options: {
-                style: 'expanded'
+                //bundleExec: true,
+                config: 'scss-lint.yml',
+                //reporterOutput: 'scss-lint-report.xml',
+                colorizeOutput: true
             },
-            files:  [{
-                expand: true,
-                cwd: 'scss',
-                src: ['*.scss'],
-                dest: 'styles',
-                ext: '.css'
-            }]
+        },
+        
+        jslint: {
+            all: ['gruntfile.js', 'scripts/**.js']
+        },
+        
+        compileTemplates: {
+            en: "abc",
+            pt: "def"
+        },
+
+        watch: {
+          files: ["**/**.scss"],
+          tasks: ['build']
         }
-    },
+    });
 
-    watch: {
-      files: ["**/**.scss"],
-      tasks: ['sass']
-    }
-  });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-nunjucks');
+    //grunt.loadNpmTasks('grunt-scss-lint');
+    //grunt.loadNpmTasks('grunt-jslint');
+    
+    grunt.registerMultiTask('compileTemplates', 'Saves the files to be statified.', function() {
+        grunt.log.writeln(this.target + ': ' + this.data);
+        grunt.log.writeln(typeof NunJucks);
+    });
+    
+    grunt.registerTask('build', [/*'jslint', 'scsslint',*/ 'sass', 'compileTemplates'/*, 'uglify'*/]);
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
 
-  grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['build']);
 
 };
+
