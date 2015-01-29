18650141d7fc7a219e117ecd20b3d58d4c435d7c
Felipe N. Moura <felipenmoura@gmail.com>
2015-01-28 23:45:12 -0200

    [.] working on animations

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
 
-    var NunJucks = require('nunjucks');
+    var NunJucks = require('nunjucks'),
+        fs = require('fs'),
+        nunEnv = new NunJucks.FileSystemLoader(['templates']),
+        defaultLang = 'en';
     
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -53,12 +56,17 @@ module.exports = function(grunt) {
         },
         
         compileTemplates: {
-            en: "abc",
+            en: {
+                talks: JSON.parse(fs.readFileSync('_bindings/talks.json', 'utf8'))//,
+                //videos: fs.readFileSync('_bindings/videos.json', 'utf8'),
+                //tools: fs.readFileSync('_bindings/tools.json', 'utf8'),
+                //albums: fs.readFileSync('_bindings/albums.json', 'utf8')
+            },
             pt: "def"
         },
 
         watch: {
-          files: ["**/**.scss"],
+          files: ["_templates/**.html", "_bindings/**", "scss/**.scss", "scripts/**.js"],
           tasks: ['build']
         }
     });
@@ -70,8 +78,58 @@ module.exports = function(grunt) {
     //grunt.loadNpmTasks('grunt-jslint');
     
     grunt.registerMultiTask('compileTemplates', 'Saves the files to be statified.', function() {
-        grunt.log.writeln(this.target + ': ' + this.data);
-        grunt.log.writeln(typeof NunJucks);
+        
+        var idxFile = 'index.html';
+        var data = this.data;
+        var done = this.async();
+        var videosList = "";
+        data.lang= this.target;
+        
+        if(this.target != defaultLang){
+            if(!fs.existsSync(this.target)){
+                fs.mkdirSync(this.target);
+            }
+            idxFile = this.target + '/index.html';
+        }
+        
+        //http://gdata.youtube.com/feeds/api/playlists/PL2LsDS820I8Qg8xXkgXTty5BGiqAOJcVB?v=2&alt=json&callback=foo
+        var options = {
+            hostname: 'gdata.youtube.com',
+            port: 80,
+            path: '/feeds/api/playlists/PL2LsDS820I8Qg8xXkgXTty5BGiqAOJcVB?v=2&alt=json&callback=foo',
+            method: 'GET',
+            headers: { 'Content-Type': 'text/javascript' }
+        };
+        
+        function render () {
+            fs.writeFileSync(idxFile, NunJucks.render('_templates/index.html', data), 'utf8');
+            done();
+        }
+
+        if(!videosList){
+            var http = require('http');
+            var req = http.request(options, function(res) {
+                var buffer= "";
+                res.setEncoding('utf8');
+                videosList = "";
+                res.on('data', function (data) {
+                    buffer+= data;
+                });
+                res.on('end', function () {
+                    videosList = buffer.replace(/^\/\/.*|^foo\(|\);$/gm, '');
+                    videosList = JSON.parse(videosList);
+                    data.videosList = videosList.feed.entry;
+                    render();
+                });
+            });
+            req.on('error', function(e) {
+                console.log('problem with request: ' + e.message);
+            });
+            req.end();
+        }else{
+            render();
+        }
+        
     });
     
     grunt.registerTask('build', [/*'jslint', 'scsslint',*/ 'sass', 'compileTemplates'/*, 'uglify'*/]);
--- scripts/default.js
+++ scripts/default.js
@@ -1,7 +1,8 @@
 (function () {
     
     var _b= document.body,
-        currentlyShowing= false;
+        currentlyShowing= false,
+        hashData = {};
     
     function showContent (id, src, path) {
         if(currentlyShowing && currentlyShowing == id || id == 'home'){
@@ -9,24 +10,128 @@
         }
         currentlyShowing= id;
         _b.setAttribute('data-page', id);
+        setHash({
+            page: (path || src).replace(/^(\.|\/)+/g, '')
+        });
         //history.pushState({}, path || src, path || src);
     }
     
     _b.addEventListener('click', function(event){
         var target= event.target || event.srcElement,
             tag= target.tagName.toLowerCase(),
+            tmp,
             src= '#';
         
         if(tag == 'a' && target.classList.contains('local')){
             // clicked on a link that must be retrieved via Ajax
             src= target.getAttribute('href');
             
-            // TODO: take it via jQuery
+            // TODO: get it via jQuery
             showContent(src.replace(/^(\.|\/)+/, '').replace(/\/.+/, ''), src);
             
             event.preventDefault();
             event.stopPropagation();
+        }else if((tmp = target.getAttribute('data-yt-ref')) || (tmp = target.parentNode.getAttribute('data-yt-ref'))){
+            showModal({
+                type: "player",
+                bg: target.getAttribute('src'),
+                ytRef: tmp
+            });
+        }else if(target.classList.contains('closeLayerBtn')){
+            closeModal();
         }
+    });
+    
+    function getHash(url){
+        if(url.indexOf('#!') > 0){
+            url = url.split('#!')[1];
+            url = url.split('/');
+            hashData = {
+                page: url[0] || '',
+                detail: url[1] || '',
+                extra: url[2] || ''
+            };
+            _b.setAttribute('data-page', hashData.page);
+            _b.setAttribute('hash-bang-detail', hashData.detail);
+            _b.setAttribute('hash-bang-extra', hashData.extra);
+        }else{
+            _b.removeAttribute('data-page');
+            _b.removeAttribute('hash-bang-detail');
+            _b.removeAttribute('hash-bang-extra');
+            hashData = {};
+        }
+        if(!hashData.extra){
+            closeModal();
+        }
+    }
+    
+    function setHash(data){
+        var newHash = '#!' + data.page;
+        if(data.detail){
+            newHash+= '/' + data.detail;
+            if(data.extra){
+                newHash+= '/' + data.extra;
+            }
+        }
+        location.hash = newHash;
+    }
+    
+    function showPlayer(data){
+        var bg = document.getElementById('playerLayer'),
+            player = document.getElementById('playerElement');
+        bg.style.backgroundImage = 'url('+ data.bg +')';
+        player.style.backgroundImage = 'url('+ data.bg +')';
+        player.setAttribute('src', "//www.youtube.com/embed/"+data.ytRef+"?showinfo=0");
+        setHash({
+            page: hashData.page,
+            detail: hashData.detail,
+            extra: data.ytRef
+        });
+    }
+    function closePlayer(){
+        document.getElementById('playerElement').removeAttribute('src');
+        setHash({
+            page: hashData.page,
+            detail: hashData.detail,
+            extra: ''
+        });
+    }
+    
+    function showModal(data){
+        switch(data.type){
+            case 'player': {
+                showPlayer(data);
+                break;
+            }
+        }
+        _b.setAttribute('data-full-layer', data.type);
+    }
+    
+    function closeModal(){
+        var curModal = _b.getAttribute('data-full-layer');
+        switch(curModal){
+            case 'player': {
+                closePlayer();
+                break;
+            }
+        }
+        _b.removeAttribute('data-full-layer');
+    }
+    
+    window.addEventListener('keyup', function(event){
+        switch(event.keyCode){
+            case 27: { // esc
+                closeModal();
+                break;
+            }
+        }
+    });
+    
+    window.addEventListener('hashchange', function(event){
+        getHash(event.newURL);
+    });
+    window.addEventListener('load', function(event){
+        getHash(location.href);
     });
     
     window.scrollTo(0, 0);
